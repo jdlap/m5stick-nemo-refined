95fb6afaa69b7ae6e64a344248888e03ed36d804
Jacob LaPietra <jdlap13@gmail.com>
2025-03-21 15:59:12 -0400

    made new folders

--- m5stick-nemo.ino
+++ m5stick-nemo.ino
@@ -3,13 +3,13 @@
 
 // -=-=-=-=-=-=- Uncomment the platform you're building for -=-=-=-=-=-=-
 // #define STICK_C_PLUS
-// #define STICK_C_PLUS2
+ #define STICK_C_PLUS2
 // #define STICK_C
 // #define CARDPUTER
 // -=-=- Uncommenting more than one at a time will result in errors -=-=-
 
 // -=-=- NEMO Language for Menu and Portal -=- Thanks, @marivaaldo and @Mmatuda! -=-=-
-// #define LANGUAGE_EN_US
+ #define LANGUAGE_EN_US
 // #define LANGUAGE_PT_BR
 // #define LANGUAGE_IT_IT
 // #define LANGUAGE_FR_FR
